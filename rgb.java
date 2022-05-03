e07802f6a69c872c8255d94c843d585f2d396006
KonkenBonken <konrad05pettersson@gmail.com>
2022-05-03 16:23:27 +0200

    Moved code out of loop

--- rgb.java
+++ rgb.java
@@ -15,14 +15,14 @@ class rgb {
     int h = 50;
     int[] matrix = new int[w * h]; // hex number in #AARRGGBB format
 
-    for (int i = 0; i < matrix.length; i++) {
-      int clr = Integer.parseInt(args[0], 16);
-      int a = 255 * 0x1000000;
-      int r = ((clr >> 16) & 0xff) * 0x10000;
-      int g = ((clr >> 8) & 0xff) * 0x100;
-      int b = (clr & 0xff) * 0x1;
+    int clr = Integer.parseInt(args[0], 16);
+    int a = 255 * 0x1000000;
+    int r = ((clr >> 16) & 0xff) * 0x10000;
+    int g = ((clr >> 8) & 0xff) * 0x100;
+    int b = (clr & 0xff) * 0x1;
+    
+    for (int i = 0; i < matrix.length; i++)
       matrix[i] = a + r + g + b;
-    }
 
     DataBufferInt buffer = new DataBufferInt(matrix, matrix.length);
 
